29cc4b63c1d59ce582983f9f96f9d26df50ddcee
Ben Fasoli <benfasoli@gmail.com>
2017-04-19 23:12:32 -0600

    Added ncdf4 to package loads
    Former-commit-id: ef4335583d5f1bba7c3f6f9087296d1b3be31565

--- r/dependencies.r
+++ r/dependencies.r
@@ -10,5 +10,5 @@ invisible(lapply(rsc, source))
 
 if (!'lib.loc' %in% ls())
   lib.loc <- NULL
-load_libs('dplyr', 'parallel', 'raster', 'rslurm', 'uataq',
+load_libs('dplyr', 'ncdf4', 'parallel', 'raster', 'rslurm', 'uataq',
           lib.loc = lib.loc)
