a101b4069691c733e71d9261504359c3332d232a
Ben Fasoli <benfasoli@gmail.com>
2017-04-21 11:27:15 -0600

    Print simulation step IDs
    Former-commit-id: be293ed0a062f1dfc3aa9f7483d0e858899465f0

--- r/src/simulation_step.r
+++ r/src/simulation_step.r
@@ -46,6 +46,7 @@ simulation_step <- function(X, rm_dat = T, stilt_wd = getwd(), lib.loc = NULL,
                        strftime(r_run_time, tz = 'UTC',
                                 format = paste0(X, '_%Y%m%d%H_', r_long, '_',
                                                 r_lati, '_', r_zagl)))
+  message(paste('Running simulation ID:  ', basename(rundir)))
 
   # Generate PARTICLE.DAT ------------------------------------------------------
   # run_trajec determines whether to try using existing PARTICLE.DAT files or to
