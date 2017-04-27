d887738927e1bf00a616ab7aaccbc8aafd28b7a4
Ben Fasoli <benfasoli@gmail.com>
2017-04-27 15:13:12 -0600

    Ensure permute executable is loaded
    Former-commit-id: c32aa8c057b0359b9f4d738a4be2092ade127315

--- r/dependencies.r
+++ r/dependencies.r
@@ -4,6 +4,7 @@
 
 if (!'stilt_wd' %in% ls())
   stilt_wd <- getwd()
+
 rsc <- dir(file.path(stilt_wd, 'r', 'src'), pattern = '.*\\.r$', full.names = T)
 invisible(lapply(rsc, source))
 
@@ -12,3 +13,9 @@ if (!'lib.loc' %in% ls())
   lib.loc <- NULL
 load_libs('dplyr', 'parallel', 'raster', 'rslurm', 'uataq',
           lib.loc = lib.loc)
+
+
+permute_exe <- file.path(stilt_wd, 'r/src/permute.so')
+if (!file.exists(permute_exe))
+  stop('calc_footprint(): failed to find permute.so in r/src/')
+dyn.load(permute_exe)
