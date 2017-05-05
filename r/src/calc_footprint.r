4ffb3c3799862cfff9d801c9e3ddfe789e61e583
benfasoli <benfasoli@gmail.com>
2017-05-05 11:16:55 -0600

    Restructured footprint output
    Former-commit-id: d9fb590b0a39e5f22775a997ccdb0624023d1cbb

--- r/src/calc_footprint.r
+++ r/src/calc_footprint.r
@@ -11,7 +11,7 @@
 #'   from PARTICLE.rds but can be derived from PARTICLE.dat with column names
 #'   equivalent to \code{varsiwant}. Must contain colums specifying long, lati,
 #'   indx, foot, and time.
-#' @param output filename argument passed to \code{raster::writeRaster}
+#' @param output filename argument passed to \code{saveRDS}
 #' @param xmn sets grid start longitude
 #' @param xmx sets grid end longitude
 #' @param xres resolution for longitude grid
@@ -22,24 +22,25 @@
 #' @import dplyr, raster, uataq
 #' @export
 
-calc_footprint <- function(p, output = NULL,
+calc_footprint <- function(p, output = NULL, r_run_time = NULL,
+                           time_integrate = F,
                            xmn = -180, xmx = 180, xres = 0.1,
                            ymn = -90, ymx = 90, yres = xres) {
-
+  
   require(dplyr)
   require(raster)
   require(uataq)
-
+  
   np <- max(p$indx, na.rm = T)
-
+  
   glong <- seq(xmn, xmx, by = xres)
   glati <- seq(ymn, ymx, by = yres)
-
+  
   # Interpolate particle locations during initial time steps
   times <- c(seq(0, -10, by = -0.1),
              seq(-10.2, -20, by = -0.2),
              seq(-20.5, -100, by = -0.5))
-
+  
   i <- p %>%
     dplyr::select(indx, time, long, lati, foot) %>%
     filter(long >= min(glong), long <= max(glong),
@@ -54,7 +55,7 @@ calc_footprint <- function(p, output = NULL,
     ungroup() %>%
     na.omit() %>%
     mutate(time = round(time, 1))
-
+  
   # Scale interpolated values to retain total field
   mi <- i$time >= -10
   mp <- p$time >= -10
@@ -65,11 +66,11 @@ calc_footprint <- function(p, output = NULL,
   mi <- i$time < -20 & i$time >= -100
   mp <- p$time < -20 & p$time >= -100
   i$foot[mi] <- i$foot[mi] / (sum(i$foot[mi], na.rm = T) / sum(p$foot[mp], na.rm = T))
-
+  
   # Remove zero influence particles and those outside of domain
   xyzt <- i %>%
     filter(foot > 0)
-
+  
   # Bootstrap pairwise distance calculation
   calc_dist <- function(x, y) {
     df <- data_frame(x, y)
@@ -80,12 +81,12 @@ calc_footprint <- function(p, output = NULL,
     }
     mean(bootstrap(df, foo, size = 50, iter = 4), na.rm = T)
   }
-
+  
   pd <- xyzt %>%
     group_by(time) %>%
     summarize(dist = calc_dist(long, lati),
               lati = mean(lati, na.rm = T))
-
+  
   # Generate gaussian kernels
   make_gauss_kernel <- function (rs, sigma) {
     # Modified from raster:::.Gauss.weight()
@@ -103,22 +104,22 @@ calc_footprint <- function(p, output = NULL,
     m <- matrix(m, ncol = nx, nrow = ny, byrow = TRUE)
     m/sum(m)
   }
-
+  
   # Gaussian kernel bandwidth scaling
   calc_bandwidth <- function(dist, lati, xyres) {
     dist / (40 * cos(lati * pi/180)) + max(xyres) / 8
   }
-
+  
   xyres <- c(xres, yres)
-
+  
   # Determine maximum kernel size
   max_k <- make_gauss_kernel(xyres, calc_bandwidth(max(pd$dist), min(pd$lati), xyres))
   xbuf <- (ncol(max_k) - 1) / 2
   ybuf <- (nrow(max_k) - 1) / 2
-
+  
   max_glong <- seq(xmn - (xbuf*xres), xmx + (xbuf*xres), by = xres)
   max_glati <- seq(ymn - (xbuf*xres), ymx + (xbuf*xres), by = yres)
-
+  
   # Pre grid particle locations
   xyzt <- xyzt %>%
     transmute(loi = as.integer(findInterval(long, max_glong)),
@@ -128,50 +129,76 @@ calc_footprint <- function(p, output = NULL,
     group_by(loi, lai, time) %>%
     summarize(foot = sum(foot, na.rm = T)) %>%
     ungroup()
-
+  
   grd <- matrix(0, ncol = length(max_glong), nrow = length(max_glati))
-
+  
   # Build gaussian kernels by time step
-  gk <- sapply(pd$time, simplify = 'array', function(x) {
+  foot <- sapply(pd$time, simplify = 'array', function(x) {
     step <- xyzt %>%
       filter(time == x)
-
+    
     # Dispersion kernel
     idx <- pd$time == x
     d <- calc_bandwidth(pd$dist[idx], pd$lati[idx], xyres)
     k <- make_gauss_kernel(xyres, d)
-
+    
     # Array dimensions
     len <- nrow(step)
     nkx <- ncol(k)
     nky <- nrow(k)
     nax <- ncol(grd)
     nay <- nrow(grd)
-
+    
     # Call permute fortran subroutine to build and aggregate kernels
     out <- .Fortran('permute', ans = grd, nax = nax, nay = nay,
                     k = k, nkx = nkx, nky = nky,
                     len = len, lai = step$lai, loi = step$loi, foot = step$foot)
-
+    
     foot <- out$ans
     return(foot)
   })
-
-  # Sum footprint 3d array across 3rd dimension
-  foot <- apply(gk, c(1, 2), sum) / np
-  footr <- cbind(expand.grid(long = max_glong + xres/2, lati = max_glati + yres/2),
-                 foot = c(foot)) %>%
-    rasterFromXYZ(crs = '+proj=longlat +ellps=WGS84')
-
-  # Crop raster to original xmn, xmx, ymn, ymx
-  footr <- crop(footr, extent(xmn, xmx, ymn, ymx))
-
-  if (!is.null(output)) {
-    if (grepl('rds', tail(unlist(strsplit(output, '.', fixed = T)), 1),
-              ignore.case = T)) {
-      saveRDS(footr, output)
-    } else raster::writeRaster(footr, output, overwrite = T)
+  
+  size <- dim(foot)
+  
+  # Flip matrix vertically to align with lati domain and fill by row for
+  # compatibility with raster
+  for (i in 1:size[3]) {
+    foot[size[1]:1, ,i] <- matrix(foot[ , ,i], byrow = T,
+                                  nrow = size[1], ncol = size[2])
   }
-
-  return(footr)
+  
+  # Subset domain to extent specified with xmn/xmx/ymn/ymx
+  foot <- foot[max_glati <= ymx & max_glati > ymn,
+               max_glong < xmx & max_glong >= xmn, ] / np
+  
+  # Time integrate footprint by summing across 3rd dimension
+  if (time_integrate) {
+    foot <- apply(foot, c(1, 2), sum)
+    time_out <- list(unit = 'Time Integrated',
+                     data = NULL)
+    foot_dims <- c('lati', 'long')
+  } else {
+    time_out <- list(unit = 'POSIXct Time',
+                     data = r_run_time + pd$time*60)
+    foot_dims <- c('lati', 'long', 'time')
+  }
+  
+  # Format output object
+  foot <- list(
+    lati = list(unit = 'Degrees',
+                res = yres,
+                extent = c(ymn, ymx),
+                data = rev(head(glati, -1))),
+    long = list(unit = 'Degrees',
+                res = xres,
+                extent = c(xmn, xmx),
+                data = head(glong, -1)),
+    time = time_out,
+    foot = list(unit = 'ppm umol-1 m2 s',
+                dims = foot_dims, data = foot)
+  )
+  
+  saveRDS(foot, output)
+  
+  return(foot)
 }
--- r/src/simulation_step.r
+++ r/src/simulation_step.r
@@ -10,12 +10,12 @@ simulation_step <- function(X, rm_dat = T, stilt_wd = getwd(), lib.loc = NULL,
                             ndump = 0, nturb = 0, numpar = 100, outdt = 0,
                             outfrac = 0.9, run_trajec = T, random = 1,
                             r_run_time, r_lati, r_long, r_zagl,
-                            tlfrac = 0.1, tratio = 0.9,
+                            time_integrate = F, tlfrac = 0.1, tratio = 0.9,
                             varsiwant = NULL, veght = 0.5, w_option = 0,
                             winderrtf = 0, zicontroltf = 0, z_top = 25000,
                             xmn = -180, xmx = 180, xres = 0.1,
                             ymn = -90, ymx = 90, yres = xres) {
-
+  
   # If using lapply or parLapply, receptors are passed as vectors and need to
   # be subsetted for the specific simulation index
   if (!slurm) {
@@ -26,7 +26,7 @@ simulation_step <- function(X, rm_dat = T, stilt_wd = getwd(), lib.loc = NULL,
   }
   # Ensure dependencies are loaded for current node/process
   source(file.path(stilt_wd, 'r/dependencies.r'), local = T)
-
+  
   if (is.null(varsiwant)) {
     varsiwant <- c('time', 'indx', 'long', 'lati', 'zagl', 'sigw', 'tlgr',
                    'zsfc', 'icdx', 'temp', 'samt', 'foot', 'shtf', 'tcld',
@@ -35,7 +35,7 @@ simulation_step <- function(X, rm_dat = T, stilt_wd = getwd(), lib.loc = NULL,
   } else if (any(grepl('/', varsiwant))) {
     varsiwant <- unlist(strsplit(varsiwant, '/', fixed = T))
   }
-
+  
   # Creates subdirectories in out for each model run time. Each of these
   # subdirectories is populated with symbolic links to the shared datasets below
   # and a run-specific SETUP.CFG and CONTROL
@@ -45,31 +45,31 @@ simulation_step <- function(X, rm_dat = T, stilt_wd = getwd(), lib.loc = NULL,
                                                 r_lati, '_', r_zagl)))
   uataq::br()
   message(paste('Running simulation ID:  ', basename(rundir)))
-
+  
   # Generate PARTICLE.DAT ------------------------------------------------------
   # run_trajec determines whether to try using existing PARTICLE.DAT files or to
   # recycle existing files
   output <- list()
   output$runtime <- r_run_time
-  output$file <- file.path(rundir, 'STILT_OUTPUT.rds')
+  output$file <- file.path(rundir, paste0(basename(rundir), '_traj.rds'))
   output$receptor <- data_frame(run_time = r_run_time,
                                 lati = r_lati,
                                 long = r_long,
                                 zagl = r_zagl)
-
+  
   if (run_trajec) {
     dir.create(rundir)
     link_files <- c('ASCDATA.CFG', 'CONC.CFG', 'hymodelc',
                     'LANDUSE.ASC', 'ROUGLEN.ASC')
     file.symlink(file.path(file.path(stilt_wd, 'exe'), link_files), rundir)
-
+    
     # Find met files necessary for simulation ------------------------------------
     met_files <- find_met_files(r_run_time, met_file_format, n_hours, met_loc)
     if (length(met_files) < 1) {
       message('No meteorological data files found...')
       return(NULL)
     }
-
+    
     write_setup(numpar, delt, tratio, isot, tlfrac, ndump, random, outdt, nturb,
                 veght, outfrac, iconvect, winderrtf, zicontroltf, mgmin,
                 varsiwant, file.path(rundir, 'SETUP.CFG'))
@@ -78,12 +78,12 @@ simulation_step <- function(X, rm_dat = T, stilt_wd = getwd(), lib.loc = NULL,
     write_runhymodelc(file.path(rundir, 'runhymodelc.bat')) %>%
       paste('bash', .) %>%
       system()
-
+    
     pf <- file.path(rundir, 'PARTICLE.DAT')
-
+    
     n_lines <- uataq::count_lines(pf)
     if (n_lines < 2) return()
-
+    
     particle <- read_particle(file = pf, varsiwant = varsiwant)
     output$particle <- particle
     saveRDS(output, output$file)
@@ -101,17 +101,18 @@ simulation_step <- function(X, rm_dat = T, stilt_wd = getwd(), lib.loc = NULL,
     }
     particle <- readRDS(output$file)$particle
   }
-
-
+  
+  
   # Produce FOOTPRINT.rds ------------------------------------------------------
   # Aggregate the particle trajectory into surface influence footprints. This
-  # outputs a FOOTPRINT.rds file, which can be read with readRDS() containing
-  # the resultant footprint and various attributes
-  f <- list()
-  f$data <- calc_footprint(particle,
-                           output = file.path(rundir,paste0(basename(rundir),
-                                                            '_foot.rds')),
-                           xmn = xmn, xmx = xmx, xres = xres,
-                           ymn = ymn, ymx = ymx, yres = yres)
+  # outputs a .rds file, which can be read with readRDS() containing the
+  # resultant footprint and various attributes
+  foot <- calc_footprint(particle,
+                         output = file.path(rundir, paste0(basename(rundir),
+                                                           '_foot.rds')),
+                         r_run_time = r_run_time,
+                         time_integrate = time_integrate,
+                         xmn = xmn, xmx = xmx, xres = xres,
+                         ymn = ymn, ymx = ymx, yres = yres)
   return(T)
 }
