b4ab0dc29cd90dcf2ca9b03ed887beaf02687adb
benfasoli <benfasoli@gmail.com>
2017-05-22 12:11:56 -0600

    Added hymodelc timeout option
    Former-commit-id: 05135a1d93948c3450f37f5ddf5f8782e8e6272e

--- r/src/write_runhymodelc.r
+++ r/src/write_runhymodelc.r
@@ -1,18 +1,18 @@
-#' write_runhymodelc writes a runhymodelc.bat batch file to execute model
+#' write_runhymodelc writes a runhymodelc.sh batch file to execute model
 #' @author Ben Fasoli
 #'
-#' @param file full path to desired runhymodelc.bat file that contains a
+#' @param file full path to desired runhymodelc.sh file that contains a
 #'   hymodelc executable
 #'
 #' @export
 
 write_runhymodelc <- function(file) {
 
-  if (basename(file) != 'runhymodelc.bat')
-    stop('write_runhymodelc(): file argument must end with runhymodelc.bat')
+  if (basename(file) != 'runhymodelc.sh')
+    stop('write_runhymodelc(): file argument must end with runhymodelc.sh')
 
   txt  <- c(paste('cd', dirname(file)),
-            './hymodelc &> hymodelc.out')
+            './hymodelc &> hymodelc.out & echo $!')
 
   write(txt, file)
   return(file)
