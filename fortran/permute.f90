e228c0af2aa04482c153a62576caf2630234e9b7
benfasoli <benfasoli@gmail.com>
2017-05-25 12:09:29 -0600

    Widened domain buffer and bug fixes
    Former-commit-id: dcf4144a67edf77590b17e5dfa50d1c2544e6189

--- fortran/permute.f90
+++ fortran/permute.f90
@@ -9,22 +9,22 @@ subroutine permute(ans, nax, nay, k, nkx, nky, len, lai, loi, foot)
   integer                       :: lox(nkx), loy(nky)
   integer                       :: i, j, n, xs, ys
   double precision              :: ks
-  double precision, intent(out) :: ans(nax, nay)
+  double precision, intent(out) :: ans(nay, nax)
 
   ! Generate location index array
   lox = (/(i, i=1,nkx)/) - ((nkx + 1) / 2)
   loy = (/(i, i=1,nky)/) - ((nky + 1) / 2)
 
   ! Matrix permutations corresponding to gaussian kernel size
-  do i = 1, nkx
-    do j = 1, nky
-      xs = lox(i)
-      ys = loy(j)
+  do i = 1, nky
+    do j = 1, nkx
+      xs = lox(j)
+      ys = loy(i)
       ks = k(i, j)
 
       ! Add all footprint values to their locations in arrays
       do n = 1, len
-        ans(loi(n)+xs, lai(n)+ys) = ans(loi(n)+xs, lai(n)+ys) + foot(n)*ks
+        ans(lai(n)+ys, loi(n)+xs) = ans(lai(n)+ys, loi(n)+xs) + foot(n)*ks
       end do
     end do
   end do
