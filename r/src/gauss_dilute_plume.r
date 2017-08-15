a880accc2f6929583c72e5669af06b7165cdb761
Ben Fasoli <benfasoli@gmail.com>
2017-08-15 12:35:03 -0600

    Methods for hyper near-field dilution
    Former-commit-id: 3213862de64176dcc004be1a6867da2da8ebcca0

---
+++ r/src/gauss_dilute_plume.r
@@ -0,0 +1,44 @@
+#' gauss_dilute_plume modifies foot influence by recalculating dilution depth
+#'   using estimated gaussian plume height based on average vertical velocity
+#'   standard deviations at each time step
+#' @author Ben Fasoli
+#' 
+#' @param p particle data frame including named dens, tlgr, sigw, and foot
+#'   columns
+#' @param numpar number of particles in simulation
+#' @param r_zagl receptor height above ground, in meters
+#' @param veght fraction of pbl height below which particle is counted
+#'
+#' @import dplyr
+#' @export
+
+gauss_dilute_plume <- function(p, numpar, r_zagl, veght) {
+  
+  # Calculate gaussian plume height using vertical velocity standard deviations
+  p <- p %>%
+    mutate(sigma = sqrt(2) * sigw * sqrt(tlgr*abs(time*60) +
+                                           tlgr^2 * exp(-abs(time*60)/tlgr)-1),
+           pbl_mixing = veght * mlht,
+           gauss_plume = r_zagl + sigma)
+  
+  # Compare average gaussian plume height with dilution height calculated at
+  # each time stepusing veght * mlht, or the fraction of the mixed layer height
+  # (defaults to veght = 0.5) to apply the influence of a particle
+  dilute <- p %>%
+    group_by(time) %>%
+    summarize_at(c('pbl_mixing', 'gauss_plume'),
+                 funs(mean(., na.rm = T))) %>%
+    arrange(-time) %>%
+    mutate(gauss_plume = cumsum(gauss_plume),
+           mask = gauss_plume < pbl_mixing)
+  
+  # Recalculate near-field foot values until the average gaussian plume height
+  # grows to veght * mlht
+  if (dilute$mask[1]) {
+    dilute_recalc_end_time <- dilute$time[which(!dilute$mask)[1] - 1]
+    mask <- p$time >= dilute_recalc_end_time
+    p$foot[mask] <- (0.02884) / (p$gauss_plume[mask] * p$dens[mask]) * p$samt[mask]*60
+  }
+  
+  return(p)
+}
