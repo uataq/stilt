5c126da385cf2ee29e2aeeff13eae19a92017fd1
Ben Fasoli <benfasoli@gmail.com>
2020-02-03 08:11:27 -0800

    Refactors tests to ensure explicit failures

---
+++ test/test_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Ben Fasoli
+set -e
+
+# Execute setup script to install stilt
+./setup
+
+echo "Setup successful."
---
+++ test/test_stilt_cli.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Ben Fasoli
+# Integration testing for STILT R wrapper
+set -e
+
+chmod +x r/stilt_cli.r
+
+# Run CLI single shot simulation
+echo "Running r/stilt_cli.r"
+r/stilt_cli.r \
+  r_run_time=2015-12-10T00:00:00Z \
+  r_lati=40.5 \
+  r_long=-112.0 \
+  r_zagl=5 \
+  met_loc=$(pwd)/stilt-tutorials/01-wbb/met \
+  met_file_format=%Y%m%d.%Hz.hrrra \
+  n_hours=-12 \
+  xmn=-113 \
+  xmx=-111 \
+  xres=0.01 \
+  ymn=39.5 \
+  ymx=41.5 \
+  yres=0.01
+
+# Check output
+model_output=$(ls out/by-id/2015121000_-112_40.5_5/2015121000_-112_40.5_5* | wc -l)
+if [ $model_output -lt 2 ]; then
+  echo "Model output not found."
+
+  echo "run_stilt.r configuration:"
+  cat r/run_stilt.r
+  
+  echo "log.txt:"
+  cat out/by-id/2015121000_-112_40.5_5/log.txt
+  exit 1
+fi
+
+# Print contents of output directory
+echo "out/by-id/<id> contents:"
+ls -lh out/by-id/2015121000_-112_40.5_5
+
+# Teardown
+rm out/by-id/2015121000_-112_40.5_5/*
+rm out/footprints/*
+rm out/particles/*
+
+echo "Model test successful."
